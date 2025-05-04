d446d88004eeff8fe53e66f14d5fd5eecd44dd16
Threnx <kingnothing46@hotmail.com>
2025-05-04 18:22:13 +0100

    Add lesson.js and all model files

--- models/lesson.js
+++ models/lesson.js
@@ -2,22 +2,10 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../db');
 
 const Lesson = sequelize.define('lesson', {
-  date: {
-    type: DataTypes.DATE,
-    allowNull: false
-  },
-  endDate: {
-    type: DataTypes.DATE,
-    allowNull: true
-  },
-  status: {
-    type: DataTypes.STRING,
-    defaultValue: 'upcoming'
-  },
-  notes: {
-    type: DataTypes.TEXT,
-    allowNull: true
-  }
+  date: { type: DataTypes.DATE, allowNull: false },
+  endDate: { type: DataTypes.DATE, allowNull: true },
+  status: { type: DataTypes.STRING, defaultValue: 'upcoming' },
+  notes: { type: DataTypes.TEXT, allowNull: true }
 });
 
 module.exports = Lesson;
