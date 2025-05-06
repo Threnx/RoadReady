0100371ad6bbe0246e33eb924c497a8654c0b67d
Threnx <kingnothing46@hotmail.com>
2025-05-06 18:27:34 +0100

    Adding Maps

--- models/user.js
+++ models/user.js
@@ -1,5 +1,11 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../db');
+const NodeGeocoder = require('node-geocoder');
+
+// Geocoder configuration (using OpenStreetMap)
+const geocoder = NodeGeocoder({
+  provider: 'openstreetmap'
+});
 
 const User = sequelize.define('user', {
   name: { type: DataTypes.STRING, allowNull: false },
@@ -9,12 +15,29 @@ const User = sequelize.define('user', {
   carType: { type: DataTypes.STRING, defaultValue: null },
   lessonPrice: { type: DataTypes.DECIMAL(10,2), defaultValue: null },
   postcode: { type: DataTypes.STRING, defaultValue: null },
+  latitude: { type: DataTypes.FLOAT, defaultValue: null },
+  longitude: { type: DataTypes.FLOAT, defaultValue: null },
   onHoliday: { type: DataTypes.BOOLEAN, defaultValue: false },
   availability: { type: DataTypes.TEXT, defaultValue: null },
   active: { type: DataTypes.BOOLEAN, defaultValue: true },
   xp: { type: DataTypes.INTEGER, defaultValue: 0 },
   level: { type: DataTypes.INTEGER, defaultValue: 1 },
   badges: { type: DataTypes.TEXT, defaultValue: null }
+});
+
+// Hook: geocode postcode before saving
+User.beforeSave(async (user, options) => {
+  if (user.changed('postcode') && user.postcode) {
+    try {
+      const res = await geocoder.geocode(user.postcode);
+      if (res && res.length > 0) {
+        user.latitude = res[0].latitude;
+        user.longitude = res[0].longitude;
+      }
+    } catch (err) {
+      console.error('Geocoding failed for postcode', user.postcode, err);
+    }
+  }
 });
 
 module.exports = User;
